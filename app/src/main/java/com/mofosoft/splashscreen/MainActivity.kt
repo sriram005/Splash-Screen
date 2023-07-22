5f2e61dba3b46d03533474b6043861304fdb9619
Sriramakrishnan S <sriramakrishnan005@gmail.com>
2023-07-22 22:29:41 +0530

    Splash Screen Implemented

--- app/src/main/java/com/mofosoft/splashscreen/MainActivity.kt
+++ app/src/main/java/com/mofosoft/splashscreen/MainActivity.kt
@@ -3,44 +3,36 @@ package com.mofosoft.splashscreen
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.mofosoft.splashscreen.ui.theme.SplashScreenTheme
 
 class MainActivity : ComponentActivity() {
+
+    private val viewModel:MainViewModel by viewModels()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        installSplashScreen().apply {
+            setKeepOnScreenCondition{
+                viewModel.isLoading.value
+            }
+
+        }
         setContent {
             SplashScreenTheme {
                 // A surface container using the 'background' color from the theme
-                Surface(
+                Box(
                     modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
+                    contentAlignment = Alignment.Center
                 ) {
-                    Greeting("Android")
+                    Text(text = "Hello World!")
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    SplashScreenTheme {
-        Greeting("Android")
     }
 }
